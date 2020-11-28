7f099ffe968da52d7702aa9b9a198943cc440352
laherasif <laherasif@gmail.com>
2020-11-28 03:10:48 -0800

    new change

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,103 @@
+import React, {useState} from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import fetch from 'isomorphic-unfetch'
+import dayjs from 'dayjs'
+import Result from '../components/result';
+import MCTForm from '../components/mctform';
 
-export default function Home() {
+const Home = ({data}) => {
+  const [results, setResults] = useState(data);
+
+  const onChange = (e) => {
+    const data = { ...results };
+
+    let name = e.target.name;
+
+    let resultType = name.split(" ")[0].toLowerCase();
+    let resultMacro = name.split(" ")[1].toLowerCase();
+
+    data[resultMacro][resultType] = parseInt(e.target.value);
+
+    setResults(data);
+  }
+
+  const getDataForPreviousDay = async () => {
+    let currentDate = dayjs(results.date);
+    let newDate = currentDate.subtract(1, 'day').format('YYYY-MM-DDTHH:mm:ss')
+    const res = await fetch('http://localhost:3000/api/daily?date=' + newDate)
+    const json = await res.json()
+    
+    setResults(json);
+  }
+
+  const getDataForNextDay = async () => {
+    let currentDate = dayjs(results.date);
+    let newDate = currentDate.add(1, 'day').format('YYYY-MM-DDTHH:mm:ss')
+    const res = await fetch('http://localhost:3000/api/daily?date=' + newDate)
+    const json = await res.json()
+    
+    setResults(json);
+  }
+
+  const updateMacros = async () => {
+    const res = await fetch('http://localhost:3000/api/daily', {
+      method: 'post',
+      body: JSON.stringify(results)
+    })
+
+    console.log(res);
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+    </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <div className="container mx-auto">
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex text-center">
+        <div className="w-full m-4">
+          <h1 className="text-4xl">Macro Compliance Tracker</h1>
         </div>
-      </main>
+      </div>
+      
+      <div className="flex text-center">
+        <div className="w-1/3 bg-gray-200 p-4"><button onClick={getDataForPreviousDay}>Previous Day</button></div>
+        <div className="w-1/3 p-4">{dayjs(results.date).format('MM/DD/YYYY')}</div>
+        <div className="w-1/3 bg-gray-200 p-4"><button onClick={getDataForNextDay}>Next Day</button></div>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className="flex mb-4 text-center">
+        <Result results={results.calories} />
+        <Result results={results.carbs} />
+        <Result results={results.fat} />
+        <Result results={results.protein} />
+      </div>
+
+      <div className="flex">
+        <MCTForm data={results} item="Total" onChange={onChange} />
+        <MCTForm data={results} item="Target" onChange={onChange} />
+        <MCTForm data={results} item="Variant" onChange={onChange} />
+      </div>
+
+      <div className="flex text-center">
+        <div className="w-full m-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={updateMacros}>
+              Save
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  </div>
+)}
+
+Home.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/daily')
+  const json = await res.json()
+  return { data: json }
 }
+
+export default Home
